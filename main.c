4af99636351de7a1d600552b1b02fe6aab308744
Rajasekaran S <proffraja@gmail.com>
2025-05-30 00:40:12 +0530

    display_account function created and tested

--- main.c
+++ main.c
@@ -13,11 +13,13 @@ account* create_account(int number, char* name){
     new_account->balance = 0.0;
     return new_account;
 }
-int main(){
-    account* an_account = create_account(1,"raja");
-
+void display_account(account* an_account){
     printf("Account No: %d\n", an_account->number);
     printf("Name      : %s\n", an_account->name);
-    printf("Balance   : %d\n",an_account->balance);
+    printf("Balance   : %d\n\n",an_account->balance);
+}
+int main(){
+    account* an_account = create_account(1,"raja");
+    display_account(an_account);
     return 0;
 }
