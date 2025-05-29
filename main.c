0db0eb2e166db1b7c9f00b7d7c00622b41cf16e5
Rajasekaran S <proffraja@gmail.com>
2025-05-30 00:48:16 +0530

    deposit_account function created & tested, display_account balance datatype is fixed

--- main.c
+++ main.c
@@ -16,10 +16,16 @@ account* create_account(int number, char* name){
 void display_account(account* an_account){
     printf("Account No: %d\n", an_account->number);
     printf("Name      : %s\n", an_account->name);
-    printf("Balance   : %d\n\n",an_account->balance);
+    printf("Balance   : %.2f\n\n",an_account->balance);
+}
+void deposit_account(account* an_account, int amount){
+    an_account->balance += amount;
+    printf("The amount %d is deposited in the account\n", amount);
+    display_account(an_account);
 }
 int main(){
     account* an_account = create_account(1,"raja");
     display_account(an_account);
+    deposit_account(an_account, 1000);
     return 0;
 }
